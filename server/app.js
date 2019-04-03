646d65995d22ba873a9468cd08dc78b0756f6574
William Bille Meyling <williambillemeyling@gmail.com>
2019-04-03 10:09:01 +0200

    wdwd

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/speech-to-text', (req, res) => {
-  res.end("")
+app.get('/', (req, res) => {
+  res.end("hwdwhbd")
 });
 
 app.listen(port, () => console.log("Listining on port " + port))
